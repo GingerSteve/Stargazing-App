33b2a97fe4a9e6cbd890ddf02fa4d86e1caf1174
Steve Marshall <thestevemarshall@gmail.com>
2017-10-01 14:14:04 -0500

    Added camera deceleration

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,9 +17,11 @@ public class CameraController : MonoBehaviour
     Sprite _compass, _arrows;
 
     float _mouseSensitivity = 70f;
-    float _touchSensitivity = 3.5f;
+    float _touchSensitivity = 3f;
     float _rotationX = 0.0f;
     float _rotationY = 0.0f;
+    float _velocityX, _velocityY;
+    float _deceleration = 1.1f;
 
 
     // Initialize controller
@@ -65,29 +67,25 @@ public class CameraController : MonoBehaviour
         {
             if (Input.GetMouseButton(0))
             {
-                //TODO lerp a little, for fast movements
+                _velocityX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+                _velocityY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
 
-                _rotationX -= Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
-                _rotationY -= Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
-                _rotationY = Mathf.Clamp(_rotationY, -90, 90);
-
-                transform.rotation = _origRotation;
-                transform.rotation *= Quaternion.AngleAxis(_rotationX, Vector3.up);
-                transform.rotation *= Quaternion.AngleAxis(_rotationY, Vector3.left);
+                RotateCamera();
             }
+            else
+                DecelerateCamera();
         }
         else if (_mode == ControlMode.Touch)
         {
             if (Input.touches.Length > 0)
             {
-                _rotationX -= Input.touches[0].deltaPosition.x * Time.deltaTime * _touchSensitivity;
-                _rotationY -= Input.touches[0].deltaPosition.y * Time.deltaTime * _touchSensitivity;
-                _rotationY = Mathf.Clamp(_rotationY, -90, 90);
+                _velocityX = Input.touches[0].deltaPosition.x * Time.deltaTime * _touchSensitivity;
+                _velocityY = Input.touches[0].deltaPosition.y * Time.deltaTime * _touchSensitivity;
 
-                transform.rotation = _origRotation;
-                transform.rotation *= Quaternion.AngleAxis(_rotationX, Vector3.up);
-                transform.rotation *= Quaternion.AngleAxis(_rotationY, Vector3.left);
+                RotateCamera();
             }
+            else
+                DecelerateCamera();
         }
         else if (_mode == ControlMode.Gyro)
         {
@@ -100,6 +98,61 @@ public class CameraController : MonoBehaviour
         }
     }
 
+    /// <summary> Rotates the camera by the X and Y velocity </summary>
+    void RotateCamera()
+    {
+        _rotationX -= _velocityX;
+        _rotationY -= _velocityY;
+        _rotationY = Mathf.Clamp(_rotationY, -90, 90);
+
+        transform.rotation = _origRotation;
+        transform.rotation *= Quaternion.AngleAxis(_rotationX, Vector3.up);
+        transform.rotation *= Quaternion.AngleAxis(_rotationY, Vector3.left);
+    }
+
+    /// <summary> Decreases the X and Y velocity, and rotates the camera </summary>
+    void DecelerateCamera()
+    {
+        //If the mouse is done moving, decelerate until stopping
+
+        bool move = false;
+
+        // Get the new speed (without direction), after deceleration
+        var newVelX = Mathf.Abs(_velocityX) / _deceleration;
+
+        // If the speed is positive, the camera will move
+        if (newVelX > 0)
+        {
+            // Get the velocity (with direction)
+            if (_velocityX < 0)
+                newVelX *= -1;
+            _velocityX = newVelX;
+
+            move = true;
+        }
+        else // If the speed is negative or 0, set the velocity to 0
+            _velocityX = 0;
+
+        // Do the same for the Y axis
+        var newVelY = Mathf.Abs(_velocityY) / _deceleration;
+        if (newVelY > 0)
+        {
+            if (_velocityY < 0)
+                newVelY *= -1;
+            _velocityY = newVelY;
+
+            move = true;
+        }
+        else
+            _velocityY = 0;
+
+        if (move)
+        {
+            RotateCamera();
+        }
+    }
+
+    /// <summary> Toggle the control mode between gyroscope controls and touch controls </summary>
     public void ToggleMode()
     {
         _mode = _mode == ControlMode.Gyro ? ControlMode.Touch : ControlMode.Gyro;
@@ -107,6 +160,8 @@ public class CameraController : MonoBehaviour
         SetImageSource();
     }
 
+
+    /// <summary> Set the 'toggle modes' button image </summary>
     public void SetImageSource()
     {
         if (_mode == ControlMode.Gyro)
